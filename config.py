f796a3d9db886fc78275e2652f578c2aab0ccf04
enrique28-ai <enriqueprecia@gmail.com>
2025-02-11 21:22:46 -0800

    Updated config.py with new changes

--- config.py
+++ config.py
@@ -3,11 +3,4 @@ import os
 
 load_dotenv()
 
-user = os.getenv('MYSQL_USER')
-password = os.getenv('MYSQL_PASSWORD')
-host = os.getenv('MYSQL_HOST')
-database = os.getenv('MYSQL_DATABASE')
-port = os.getenv('MYSQL_PORT')
-
-
-DATABASE_CONNECTION_URI = f"mysql://{user}:{password}@{host}:{port}/{database}"
+DATABASE_CONNECTION_URI = os.getenv("MYSQL_URL")
