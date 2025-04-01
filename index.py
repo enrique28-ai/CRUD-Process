ae9552e1e5d5ff996e695c6bbb6058403c5ccd39
enrique28-ai <enriqueprecia@gmail.com>
2025-04-01 00:34:27 -0700

    agregar puerto

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
 from app import app
 from utils.db import db
-
+import os
 with app.app_context():
     db.create_all()
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
