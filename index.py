bdde9af28f806023c5674fb1615895734e50bb55
enrique28-ai <enriqueprecia@gmail.com>
2025-04-01 15:23:23 -0700

    modify index

--- index.py
+++ index.py
@@ -1,9 +1,7 @@
 from app import app
 from utils.db import db
-import os
 with app.app_context():
     db.create_all()
     
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
